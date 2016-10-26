8822fe3a117152812a66dbaa8656c6e7d5b2320a
Connor leslie Giannotti <845862604@kent-202-16m.local>
2016-10-26 15:20:46 +1300

    added teplates folder for php

---
+++ templates/home.inc.php
@@ -0,0 +1,73 @@
+<?php
+
+function content(){
+?>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Schlockoktoberfest <small>The Best Worst Movie Festival Ever!</small></h1>
+			</div>
+		</div>
+		<div class="row">
+  			<div class="col-sm-6">
+  			<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  			tempor incididunt ut labore et dolore magna aliqua. </p>
+  			<button type="button" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-film"></span> Film Programme</button>
+  			<button type="button" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-shopping-cart"></span> Book Tickets (Coming Soon)</button>
+
+  			</div>
+  			<div class="col-sm-6">
+  				<img class="img-responsive" src="http://placekitten.com/700/400" alt="kitten image">
+  			</div>
+  			<div class="row">
+  				<div class="col-sm-4">
+  					<h3 class="text-center">Best Wosrt Movie (2009)</h3>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  					tempor incididunt ut labore et dolore magna aliqua.</p>
+  				</div>
+  				<div class="col-sm-4">
+  					<h3 class="text-center">Movie Suggest</h3>
+  					<form class="form-horizontal" method="post" action="success.html">
+					  <div class="form-group">
+					    <label for="email" class="col-sm-2 control-label">Email</label>
+					    <div class="col-sm-10">
+					      <input type="email" class="form-control" id="email" placeholder="Email">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <label for="title" class="col-sm-2 control-label">Movie Title</label>
+					    <div class="col-sm-10">
+					      <input type="text" class="form-control" id="title" placeholder="Movie Title">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <div class="col-sm-offset-2 col-sm-10">
+					      <div class="checkbox">
+					        <label>
+					          <input type="checkbox"> Subscribe for Schlockoktoberfest Newsletter <small>(Once a Month)</small>
+					        </label>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <div class="col-sm-offset-2 col-sm-10">
+					      <button type="submit" class="btn btn-primary">Suggest Movie</button>
+					    </div>
+					  </div>
+					</form>
+  				</div>
+  				<div class="col-sm-4">
+  					<h3 class="text-center">Our Sponsors</h3>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  				</div>
+  			</div>
+		</div>
+		
+<?php
+	}
+?>
